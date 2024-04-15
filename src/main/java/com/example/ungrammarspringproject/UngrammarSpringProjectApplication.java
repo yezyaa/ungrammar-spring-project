21221b2cd0de3a079527805cdfac5087b8edc1c3
yezy <iyeahji@gmail.com>
2024-04-15 15:08:21 +0900

    first commit

---
+++ src/main/java/com/example/ungrammarspringproject/UngrammarSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ungrammarspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UngrammarSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UngrammarSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ungrammarspringproject/UngrammarSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ungrammarspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UngrammarSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
